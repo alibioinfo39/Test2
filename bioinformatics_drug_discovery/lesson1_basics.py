a1d67b9a9c4568097e68a49a744119740d83ce14
Dr. Lee <lic.aragones@gmail.com>
2026-01-11 23:06:57 +0800

    011126

--- bioinformatics_drug_discovery/lesson1_basics.py
+++ bioinformatics_drug_discovery/lesson1_basics.py
@@ -24,18 +24,59 @@ print(protein_sequence[-1])   # Last
 #Dictionary = Amino Acid Properties
 aa_property = {
     "A": "Hydrophobic",
-    "E": "Negative",
+    "V": "Hydrophobic",
+    "I": "Hydrophobic",
+    "L": "Hydrophobic",
+    "M": "Hydrophobic",
+    "F": "Hydrophobic",
+    "W": "Hydrophobic",
+    "Y": "Hydrophobic",
+    
     "K": "Positive",
-    "S": "Polar"
+    "R": "Positive",
+    "H": "Positive",
+    
+    "D": "Negative",
+    "E": "Negative",
+    
+    "S": "Polar",
+    "T": "Polar",
+    "N": "Polar",
+    "Q": "Polar",
+    "C": "Polar",
+    "G": "Neutral",
+    "P": "Neutral"
 }
 
-for aa in protein_sequence:
-    if aa in aa_property:
-        print(aa, aa_property[aa])
+# for aa in protein_sequence:
+#     if aa in aa_property:
+#         print(aa, aa_property[aa])
 
 #Count Amino Acids
+sequence = "MKTAYIAKQRQISFVKSHFSRQDILDLW"
+
 aa_count = {}
 
-for aa in protein_sequence:
-    aa_count[aa] = aa_count.get(aa, 0) + 1
-print(aa_count)
+# for aa in sequence:
+#     aa_count[aa] = aa_count.get(aa, 0) + 1
+
+#Assignment Hydrophobic %
+
+
+
+print(len(sequence))
+
+hydophobic_count = 0 
+
+for aa in sequence:
+    if aa in aa_property:
+        if aa_property[aa] == "Hydrophobic":
+            hydophobic_count += 1
+
+hydrophonic_percent = (hydophobic_count/ len(sequence)) * 100
+print(f"Hydrophobic residues: {hydophobic_count}")
+print(f"Hydrophobic percentage: {hydrophonic_percent:.2f}%")
+#Hydophobic is crucial in ligand binding because it is how they drive the interaction through hydophobic effect creating a favorable environment by releasing ordered water molecules
+
+
+
